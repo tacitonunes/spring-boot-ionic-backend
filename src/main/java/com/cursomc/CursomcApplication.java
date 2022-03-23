41684bd5c6305212c9a4a4cd2497806ee9ff150b
Tácito Nunes <tacito.nunes@hotmail.com>
2022-03-23 20:11:40 -0300

    Criando operação de instanciação automática - Categoria

--- src/main/java/com/cursomc/CursomcApplication.java
+++ src/main/java/com/cursomc/CursomcApplication.java
@@ -1,13 +1,33 @@
 package com.cursomc;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.cursomc.domain.Categoria;
+import com.cursomc.respositories.CategoriaRepository;
+
 @SpringBootApplication
-public class CursomcApplication {
+public class CursomcApplication implements CommandLineRunner {
+	
+	@Autowired //Instanciação automática
+	private CategoriaRepository categoriaRespository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+		Categoria cat1 = new Categoria(null, "Informática");
+		Categoria cat2 = new Categoria(null, "Escritório");
+		
+		categoriaRespository.saveAll(Arrays.asList(cat1, cat2));
+		
 	}
 
 }
