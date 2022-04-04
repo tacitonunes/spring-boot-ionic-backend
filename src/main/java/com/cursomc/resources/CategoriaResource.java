af8be020b8b61a73d3f42be5c9224a3cbda59caf
Tácito Nunes <tacito.nunes@hotmail.com>
2022-04-04 07:30:30 -0300

    Listando todas as categorias sem produtos com DTO

--- src/main/java/com/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/cursomc/resources/CategoriaResource.java
@@ -2,6 +2,7 @@ package com.cursomc.resources;
 
 import java.net.URI;
 import java.util.List;
+import java.util.stream.Collectors;
 
 /*import java.util.ArrayList;
 import java.util.List;*/
@@ -16,6 +17,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.cursomc.domain.Categoria;
+import com.cursomc.dto.CategoriaDTO;
 import com.cursomc.services.CategoriaService;
 
 @RestController
@@ -26,10 +28,11 @@ public class CategoriaResource {
 	private CategoriaService service;
 	
 	@RequestMapping(method = RequestMethod.GET)
-	public ResponseEntity<?> listAll() {
+	public ResponseEntity<List<CategoriaDTO>> listAll() {
 
-		List<Categoria> obj = service.listAll();
-		return ResponseEntity.ok().body(obj);
+		List<Categoria> list = service.listAll();
+		List<CategoriaDTO> listDto = list.stream().map(obj -> new CategoriaDTO(obj)).collect(Collectors.toList());
+		return ResponseEntity.ok().body(listDto);
 
 	}
 	
@@ -57,7 +60,6 @@ public class CategoriaResource {
 	}
 	@RequestMapping(value="/{id}", method = RequestMethod.DELETE)
 	public ResponseEntity<Void> delete(@PathVariable Integer id){
-		
 		service.delete(id);
 		return ResponseEntity.noContent().build();
 		
